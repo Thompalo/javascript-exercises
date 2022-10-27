6f4dbcddc0fd68693c1cfe58abda245e49d6f723
Thompalo <thomas@epalo.no>
2022-10-27 22:40:03 +0200

    Start, but ...

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let buildString = "";
+    for (let i = 0; i < num; i++) {
+        
+        buildString = buildString.concat(string);
+        
+}
+return buildString;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
