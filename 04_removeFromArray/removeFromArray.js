0bd6c065d5b5a4436d4b06d17c4279eb37304f56
Thompalo <thomas@epalo.no>
2022-10-30 16:02:38 +0100

    Finish ex...

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,10 @@
 const removeFromArray = function(arr, ...args) {
-    for (var i = arr.length; i--;) {
-        arr.forEach(element => {
-            if (arr[i] === args) arr.splice(i, 1);
-        });
-       
-      }
-      return arr;
-   };
+    let forDeletion = args;
+
+    arr = arr.filter(eachItemInArr => !forDeletion.includes(eachItemInArr));
+
+    return arr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
