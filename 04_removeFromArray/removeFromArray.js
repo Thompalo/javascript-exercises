98e5ac7a4001d62aa1df6c972b20b6ab7ecd7efe
Thompalo <thomas@epalo.no>
2022-10-30 08:42:37 +0100

    Start with mulitiple values and the rest parameter...

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(arr, ...args) {
+    for (var i = arr.length; i--;) {
+        arr.forEach(element => {
+            if (arr[i] === args) arr.splice(i, 1);
+        });
+       
+      }
+      return arr;
+   };
 
 // Do not edit below this line
 module.exports = removeFromArray;
