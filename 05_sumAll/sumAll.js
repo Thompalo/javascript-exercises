bff766ef00385c96f47c98237ed4392c0b624ffc
Thompalo <thomas@epalo.no>
2022-10-30 20:49:46 +0100

    Start and first two tests passed.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
-
-};
+const sumAll = function(num1, num2) {
+    let finalValue = 0;
+    let array = [];
+        for (let i = num1; i <= num2; i++) {
+                array.push(i);
+        }
+        finalValue = array.reduce((
+            previousValue, currentValue) => previousValue + currentValue);
+    
+        return finalValue;
+    };
 
 // Do not edit below this line
 module.exports = sumAll;
