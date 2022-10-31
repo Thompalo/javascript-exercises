262e13ed9a0d47d078ffd803c23ef90c183508a5
Thompalo <thomas@epalo.no>
2022-10-31 11:13:39 +0100

    Finish

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+let celsius = 5 / 9 * (num - 32);
+celsius = Math.round(celsius * 10) / 10;
+return celsius;
 };
 
-const ctof = function() {
-
+const ctof = function(num) {
+let fahr = 32 + (num * 1.8);
+fahr = Math.round(fahr * 10) / 10;
+return fahr;
 };
 
 // Do not edit below this line
