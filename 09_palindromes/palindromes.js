be8dc6dbbf05e4c07cc15cc435254017d66e0c52
Thompalo <thomas@epalo.no>
2022-11-01 11:19:15 +0100

    Finish

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,27 @@
-const palindromes = function () {
+const palindromes = function (string) {
+let forW = string.toLowerCase().replace(/[^a-z]+/g, "");
+let backW = forW.split("").reverse().join();
+forW = forW.split("").join();
+if (forW === backW) { 
+    return true;
+} else {
+    return false;
+}
 
+//console.log(forW);
+//console.log(backW);
 };
+
+//palindromes("test!");
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
+
+/* if (forW == backW) { 
+    return true;
+} else {
+    return false;
+}
+ */
