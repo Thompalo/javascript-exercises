e48fe0859cc0a2adcbb006bd7a37f9b23322421a
Thompalo <thomas@epalo.no>
2022-11-01 09:28:38 +0100

    Finsih - with too much code...

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,14 +1,14 @@
 const sumAll = function(num1, num2) {
     let finalValue = 0;
     let array = [];
-    if (num1 < num2) {
-        temp = num1;
+    if (num1 > num2) {
+        let temp = num2;
         num2 = num1;
-        num1 = temp;
-        
-    } else {
-
+        num1 = temp; 
+    } else if (num1 < 0 || num2 < 0 || isNaN(num1) || isNaN(num2) || typeof num1 === "string" || typeof num2 === "string") {
+        return "ERROR";
     }
+    
     for (let i = num1; i <= num2; i++) {
                 array.push(i);
         }
@@ -20,3 +20,14 @@ const sumAll = function(num1, num2) {
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
+ /*   if ((num1 < 0 || num2 < 0) || (num1 == NaN || num2 == NaN)) {
+        return "ERROR";
+    }
+     else if (num1 < num2) {
+        let temp = num2;
+        num2 = num1;
+        num1 = temp; 
+    }
+    */
